99b595896cd34ad6316db5a303fe06889c5ffef3
kolev9605 <kolev9605@gmail.com>
2016-03-24 16:55:02 +0200

    Data structures homework 10

---
+++ Data Structures/Homeworks/10-DataStructuresEfficiency/03-CollectionOfProducts/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace _03_CollectionOfProducts
+{
+    using System;
+
+    public class Product : IComparable<Product>
+    {
+        public Product(string id, string title, string supplier, decimal price)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Supplier = supplier;
+            this.Price = price;
+        }
+
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Supplier { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            return this.Id.CompareTo(other.Id);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Id}: {this.Title} ot {this.Supplier} za {this.Price:F2}lv";
+        }
+    }
+}
